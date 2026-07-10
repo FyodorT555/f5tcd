fca8b4b9aef15939f8d0cf4c28903699e6d55935
ft555 <teplyakov.fedor@gmail.com>
2026-07-10 20:06:41 +0900

    v0.1.4

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 // libs
 
 #include <algorithm>
+#include <climits>
 #include <cmath>
 #include <deque>
 #include <iostream>
@@ -24,20 +25,56 @@ using namespace std;
 
 #define int long long
 #define str string 
+#define vi vector<int>
+#define pii pair<int, int>
 #define el "\n"
+
+#define fr front
+#define bc back
+#define fi first
+#define se second
+#define pp pop
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
 
 #define forf(i, a, b) for(int i = a; i < b; i++) 
 #define forb(i, a, b) for(int i = a; i > b; i--) 
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define size(x) (int)x.size()
 
 // ================================================================
 
 // constants
 
 const int MOD = 1e9 + 7;
+const double PI = 3.1415926535;
+
+// const int INF = LLONG_MAX;
+const int INF = 1e18;
 
 // ================================================================
 
 // functions
+
+int bp(int a, int s)
+{
+    if(s == 0) return 1;
+    if(s == 1) return a;
+
+    if(s % 2 == 0)
+    {
+        int n = bp(a, (int)(s / 2));
+        return n * n;
+    }
+    else
+    {
+        int n = bp(a, (int)((s - 1) / 2));
+        return n * n * a;
+    }
+}
 
 // ================================================================
 
@@ -45,9 +82,23 @@ const int MOD = 1e9 + 7;
 
 // ================================================================
 
-void rd() {}
-void wr() {}
-void lg() {}
+// void rd() {}
+// void wr() {}
+
+void lg(str s)
+{
+    cout << "[log] " << s << el;
+}
+
+// ================================================================
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b;
+}
 
 // ================================================================
 
@@ -57,7 +108,13 @@ signed main()
     cin.tie(0);
     cout.tie(0);
 
-    cout << "Hello world!\n";
+    int t = 1;
+    // cin >> t;
+
+    forf(w, 0, t)
+    {
+        solve();
+    }
 
     return 0;
 }
